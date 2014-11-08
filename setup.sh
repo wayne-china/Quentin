57dc0799c7e01cbda802407a803e487d2f785a12
wayne-china <yaoyishi@gmail.com>
2014-11-08 16:48:19 +0800

    correct apache wsgi

--- setup.sh
+++ setup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+current_path=$(cd "$(dirname "$0")"; pwd)
+install_software='django pip httpd'
+install_app='grappelli pagination markdown2'
+http_wsgi_path='/etc/httpd/conf.d/wsgi.conf'
+
+
 if which yum
 then 
-  install_software='django pip httpd'
-  install_app='grappelli pagination markdown2'
-
+# 安装必要软件
   for software in $install_software
   do yum install $software
   done
@@ -11,6 +16,16 @@ then
   for app in $install_app
   do pip install $app
   done
+# 修改apache wsgi配置中的路径 
+  file='/wsgi.py'
+  sed -i s~path~$current_path$file~g ./conf/wsgi.conf
+  if [ -x $http_wsgi_path ]
+    then
+      cat ./conf/wsgi.conf >> $http_wsgi_path
+  else
+    cp ./conf/wsgi.conf /etc/httpd/conf.d/
+# 修改app 中wsgi.py的路径
+  sed -i s~AppPath~$current_path~g ./Quentin/wsgi.py
 
 else echo 'Please install Yum packages'
 fi
