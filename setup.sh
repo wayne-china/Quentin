714af146f9dc2c07f70e5e69ae1479f7f58ce283
wayne-china <yaoyishi@gmail.com>
2014-11-08 14:32:55 +0800

    setup script

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if which yum
+then 
+  install_software='django pip httpd'
+  install_app='grappelli pagination markdown2'
+
+  for software in $install_software
+  do yum install $software
+  done
+
+  for app in $install_app
+  do pip install $app
+  done
+
+  yum install httpd 
+else echo 'Please install Yum packages'
+fi
+
