954dc3a44248855796d6ff2e02e70b110b5af052
wayne-china <yaoyishi@gmail.com>
2014-11-12 20:35:41 +0800

    for opensuse

--- setup.sh
+++ setup.sh
@@ -1,38 +1,39 @@
 #!/bin/bash
 
 current_path=$(cd "$(dirname "$0")"; pwd)
-install_software='httpd mod_wsgi'
-install_app='django==1.7 django-grappelli django-pagination django-markdown2'
-http_wsgi_path='/etc/httpd/conf.d/wsgi.conf'
+install_software='apache2 apache2-mod_wsgi'
+install_app='django==1.7 django-grappelli django-pagination django-markdown'
+http_wsgi_path='/etc/apache2/conf.d/wsgi.conf'
 
 
-if which yum
+if which zypper
 then 
 # 安装必要软件
-  sudo yum update
   for software in $install_software
-  do sudo yum install -y $software
+  do zypper install -y $software
   done
  
-  easy_install pip
+  zypper install python-pip
 
   for app in $install_app
-  do sudo pip install $app
+  do pip install $app
   done
 # 修改apache wsgi配置中的路径 
   file='/wsgi.py'
   sed -i s~path~$current_path$file~g ./conf/wsgi.conf
   if [ -x $http_wsgi_path ]
   then
-    sudo cat ./conf/wsgi.conf >> $http_wsgi_path
+    cat ./conf/wsgi.conf >> $http_wsgi_path
+    echo "/etc/apache2/conf.d/wsgi.conf changed successful"
   else
-    sudo cp ./conf/wsgi.conf /etc/httpd/conf.d/
+    cp ./conf/wsgi.conf /etc/httpd/conf.d/
+    echo "/etc/apache2/conf.d/wsgi.conf copied successful"
   fi
 # 修改app 中wsgi.py的路径
   sed -i s~AppPath~$current_path~g ./Quentin/wsgi.py
 # 启动服务
-  service httpd start
+  systemctl start apache2
 
-else echo 'Please install Yum packages'
+else echo 'Please install zypper packages'
 fi
 
